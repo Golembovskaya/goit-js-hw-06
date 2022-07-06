64774961fe4c3959346088cdefe6392de1857b10
Люда <1982luda30@gmail.com>
2022-07-07 01:03:57 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = document.querySelector("#ingredients");
+console.log(ingredientsEl);
+console.log(ingredients);
+
+const createElement = ingredients.map((el) => {
+  const createEl = document.createElement("li");
+  createEl.textContent = el;
+  createEl.classList.add("item");
+  return createEl;
+});
+
+ingredientsEl.append(...createElement);
