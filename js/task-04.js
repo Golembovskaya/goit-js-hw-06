ad71d93cb0af1429b66381325c964b176d058106
Люда <1982luda30@gmail.com>
2022-07-10 11:36:37 +0300

    bug fix task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,28 @@
-const counterValue = document.querySelector('#value');
+//const counterValue = document.querySelector('#value');
+//const decrementBtn = document.querySelector('button[data-action="decrement"]');
+//const incrementBtn = document.querySelector('button[data-action="increment"]');
+//console.log(decrementBtn.dataset.action);
+//console.log(incrementBtn.dataset.action);
+//const decrement= () => {
+   // counterValue.textContent = Number(counterValue.textContent) - 1;
+//};
+//const increment = () => {
+    //counterValue.textContent = Number(counterValue.textContent) + 1;
+//};
+//decrementBtn.addEventListener('click', decrement);
+//incrementBtn.addEventListener('click', increment);
+
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-console.log(decrementBtn.dataset.action);
-console.log(incrementBtn.dataset.action);
-const decrement= () => {
-    counterValue.textContent = Number(counterValue.textContent) - 1;
+
+let counterValue = 0;
+const decrement = () => {
+    counterValue -= 1;
+   document.getElementById("value").textContent = counterValue;
 };
 const increment = () => {
-    counterValue.textContent = Number(counterValue.textContent) + 1;
+    counterValue += 1;
+   document.getElementById("value").textContent = counterValue;
 };
 decrementBtn.addEventListener('click', decrement);
 incrementBtn.addEventListener('click', increment);
